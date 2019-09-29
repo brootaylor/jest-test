1b9a905f8d29e6bcf9b644bff7b188eac8d9b6a1
Bruce Taylor <bruce.taylor@avios.com>
2019-09-29 15:08:52 +0100

    Updated some of the tests to get passes and fails.

--- scripts/utils.esm.test.js
+++ scripts/utils.esm.test.js
@@ -2,25 +2,37 @@
 import { utils } from './utils.esm';
 
 test('Adds 4 + 5 to equal 9', () => {
-    expect(utils.add(4, 5)).toBe(9); // Passes
-    // expect(utils.add(4, 5)).toBe(10); // Fails
+    const add1 = utils.add(4, 5);
+    expect(add1).toBe(9); // Passes
+
+    const add2 = utils.add(4, 19);
+    expect(add2).toBe(9); // Fails
 });
 
 test('Adds 4 + 5 to NOT equal 9', () => {
-    expect(utils.add(4, 5)).not.toBe(11); // Passes
-    // expect(utils.add(4, 5)).not.toBe(9); // Fails
+    const add1 = utils.add(4, 5);
+    expect(add1).not.toBe(9); // Fails
+
+    const add2 = utils.add(4, 78);
+    expect(add2).not.toBe(9); // Passes
 });
 
 // toBeNull
 test('Should be null value', () => {
-    expect(utils.isNull()).toBeNull(); // Passes
+    const value = utils.isNull();
+    expect(value).toBeNull(); // Passes
 });
 
 // toBeFalsy
 test('Should be falsy value', () => {
-    // expect(utils.checkValue(null)).toBeFalsy(); // Passes
-    expect(utils.checkValue(undefined)).toBeFalsy(); // Passes
-    // expect(utils.checkValue(2)).toBeFalsy(); // Fails
+    const falsyA = utils.checkValue(null);
+    expect(falsyA).toBeFalsy(); // Passes
+
+    const falsyB = utils.checkValue(undefined);
+    expect(falsyB).toBeFalsy(); // Passes
+
+    const falsyC = utils.checkValue(2);
+    expect(falsyC).toBeFalsy(); // Fails
 });
 
 // toStrictEqual
